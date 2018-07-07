1c6ea12f1bf7a8c662c8eb30bdf148e1ff84eb70
Jet <Jetlee_001@163.com>
2018-07-07 23:21:56 +0800

    read authentication

--- default.aspx.cs
+++ default.aspx.cs
@@ -11,7 +11,8 @@ namespace azurewebportal
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.info.Text = "hello";
+            this.info.Text = Context.User.Identity.Name;
+
         }
     }
 }
